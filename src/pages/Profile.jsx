5142fc478d382820480822b1014ff17d684c8663
esmeraldo17 <esmeraldoalberto55@gmail.com>
2022-09-28 00:45:14 +0100

    requisito 13

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,11 +1,48 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Header from '../components/Header';
+import Carregando from './Carregando';
+import { getUser } from '../services/userAPI';
 
 class Profile extends Component {
+  constructor() {
+    super();
+    this.state = {
+      carregando: false,
+      info: {},
+    };
+  }
+
+  componentDidMount() {
+    this.getInfo();
+  }
+
+  getInfo = async () => {
+    this.setState({ carregando: true }, async () => {
+      const info = await getUser();
+      this.setState({ carregando: false, info });
+    });
+  };
+
   render() {
+    const { info, carregando } = this.state;
+
     return (
       <div data-testid="page-profile">
         <Header />
+        {carregando ? <Carregando /> : (
+          <div>
+            <p>{info.name}</p>
+            <p>{info.email}</p>
+            <p>{info.description}</p>
+            <img
+              src={ info.image }
+              alt=""
+              data-testid="profile-image"
+            />
+            <Link to="/profile/edit">Editar perfil</Link>
+          </div>
+        )}
       </div>
     );
   }
