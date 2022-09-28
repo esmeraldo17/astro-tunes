afd1cb7835cbc6f41a65f98ff0cf2c4ad51f132a
esmeraldo17 <esmeraldoalberto55@gmail.com>
2022-09-28 01:06:00 +0100

    resolucao do requisito 11

--- src/pages/Album.jsx
+++ src/pages/Album.jsx
@@ -3,7 +3,7 @@ import { shape, string } from 'prop-types';
 import Header from '../components/Header';
 import MusicCard from './MusicCard';
 import getMusics from '../services/musicsAPI';
-import { addSong, getFavoriteSongs } from '../services/favoriteSongsAPI';
+import { addSong, getFavoriteSongs, removeSong } from '../services/favoriteSongsAPI';
 import Carregando from './Carregando';
 
 class Album extends Component {
@@ -20,7 +20,7 @@ class Album extends Component {
 
   componentDidMount() {
     this.requisicao();
-    // this.pegaFavorito();
+    this.pegaFavorito();
   }
 
   requisicao = async () => {
@@ -48,29 +48,25 @@ class Album extends Component {
     );
   };
 
-  // removeFavorite = async (musica) => {
-  //   const {
-  //     target: { checked },
-  //   } = musica;
-  //   this.setState({ carregando: true });
-  //   // console.log(carregando);
-  //   await removeSong(musica);
-  //   this.setState({ carregando: false });
-  // };
+  removeFavorite = async (musica) => {
+    this.setState({ carregando: true }, async () => {
+      await removeSong(musica);
+      await this.pegaFavorito();
+      this.setState({ carregando: false });
+    });
+  };
 
-  // favorite = async (e) => {
-  //   const {
-  //     target: { checked },
-  //   } = e;
+  favorite = async (e) => {
+    const { favorito } = this.state;
+    const isFavorite = favorito.some((e2) => e2.trackName === e.trackName);
+    console.log(isFavorite);
 
-  //   console.log(checked);
-
-  //   if (checked) {
-  //     await this.addFavorite(e);
-  //   } else {
-  //     await this.removeFavorite(e);
-  //   }
-  // };
+    if (isFavorite) {
+      await this.removeFavorite(e);
+    } else {
+      await this.addFavorite(e);
+    }
+  };
 
   pegaFavorito = async () => {
     const favoritos = await getFavoriteSongs();
@@ -104,8 +100,8 @@ class Album extends Component {
                   type="checkbox"
                   data-testid={ `checkbox-music-${e.trackId}` }
                   onClick={ async () => {
-                    await this.addFavorite(e);
-                    // this.favorite(e);
+                    // await this.addFavorite(e);
+                    await this.favorite(e);
                     await this.pegaFavorito();
                   } }
                   checked={ favorito.length > 0 && (
